e5e15e0c1f52b5bc74ec0e45861d8c5f845acf4b
Shahidul <theshahidkhan.2004@gmail.com>
2026-01-06 00:17:33 +0600

    added problem 7

---
+++ MidPractice/Linked List/7.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+Node* reverseList(Node* head) {
+    if (!head || !head->next) return head;
+    
+    Node* prev = nullptr;
+    Node* current = head;
+    Node* next = nullptr;
+    
+    while (current) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    
+    return prev;  // New head
+}
+
+void printList(Node* head) {
+    Node* tmp = head;
+    while (tmp) {
+        cout << tmp->data;
+        if (tmp->next) cout << " -> ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    // Create a list: 1 -> 2 -> 3 -> 4 -> 5
+    Node* head = new Node{1, nullptr};
+    head->next = new Node{2, nullptr};
+    head->next->next = new Node{3, nullptr};
+    head->next->next->next = new Node{4, nullptr};
+    head->next->next->next->next = new Node{5, nullptr};
+    
+    cout << "Original list: ";
+    printList(head);
+    
+    head = reverseList(head);
+    
+    cout << "Reversed list: ";
+    printList(head);
+    
+    return 0;
+}
